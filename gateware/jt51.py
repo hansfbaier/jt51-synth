8ebe9c1dd69bbda9b08647a013f07100f8ca8396
Hans Baier <hansfbaier@gmail.com>
2021-06-03 12:52:19 +0700

    jt51.py: implement FSM to stream MIDI into the JT51

--- gateware/jt51.py
+++ gateware/jt51.py
@@ -1,7 +1,69 @@
-from nmigen              import Elaboratable, Module, Signal, Instance
+from nmigen              import Elaboratable, Module, Signal, Instance, ClockDomain
+from luna.gateware.stream import StreamInterface
+from nmigen.hdl.ast import ResetSignal
 
-class jt51(Elaboratable):
-    def __init__(self):
+class Jt51Streamer(Elaboratable):
+    def __init__(self, jt51) -> None:
+        self.input_stream = StreamInterface(payload_width=16)
+        self.jt51 = jt51
+
+    def elaborate(self, platform):
+        m = Module()
+        #m.domains.jt51 = ClockDomain("jt51")
+        jt51 = self.jt51
+
+        ready   = Signal()
+        valid   = Signal()
+        address = Signal(8)
+        data    = Signal(8)
+        busy    = Signal()
+
+        m.d.comb += [
+            self.input_stream.ready.eq(ready),
+            valid.eq(self.input_stream.valid),
+            busy.eq(jt51.dout[7]),
+        ]
+
+        with m.If(valid & ready):
+            m.d.jt51 += [
+                address.eq(self.input_stream.payload[8:]),
+                data.eq(self.input_stream.payload[:8]),
+                ready.eq(0), # only read one FIFO entry
+            ]
+
+        with m.FSM(domain="jt51"):
+            with m.State("IDLE"):
+                m.d.jt51 += jt51.wr_n.eq(1)
+
+                with m.If(self.input_stream.valid & ~busy):
+                    # read a FIFO entry
+                    m.d.jt51 += ready.eq(1)
+                    m.next = "WRITE_ADDRESS"
+
+            with m.State("WRITE_ADDRESS"):
+                m.d.jt51 += [
+                    jt51.a0.eq(0),
+                    jt51.din.eq(address),
+                    jt51.wr_n.eq(0),
+                ]
+                m.next = "ADDRESS_DONE"
+
+            with m.State("ADDRESS_DONE"):
+                m.d.jt51 += jt51.wr_n.eq(1)
+                m.next = "WRITE_DATA"
+
+            with m.State("WRITE_DATA"):
+                m.d.jt51 += [
+                    jt51.a0.eq(1),
+                    jt51.din.eq(data),
+                    jt51.wr_n.eq(0),
+                ]
+                m.next = "IDLE"
+
+        return m
+
+class Jt51(Elaboratable):
+    def __init__(self) -> None:
         self.rst      = Signal()
         self.clk      = Signal()
         self.cen      = Signal()
