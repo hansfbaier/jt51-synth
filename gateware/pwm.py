6e23156a9f366700151604f176925711a725d615
Hans Baier <hansfbaier@gmail.com>
2021-06-21 05:22:02 +0700

    add pwm module

---
+++ gateware/pwm.py
@@ -0,0 +1,18 @@
+from nmigen import *
+from nmigen.build import Platform
+
+class PWM(Elaboratable):
+	def __init__(self, width=8):
+		self.width = width
+		self.i = Signal(width)
+		self.o = Signal()
+
+	def elaborate(self, platform: Platform) -> Module:
+		m = Module()
+
+		counter = Signal(self.width)
+
+		m.d.comb += self.o.eq(counter < self.i)
+		m.d.sync += counter.eq(counter + 1)
+
+		return m
