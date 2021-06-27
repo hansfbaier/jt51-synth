b0694ce88c5bc6a9f8a9a6ace7e13ee9a5f91458
Hans Baier <hansfbaier@gmail.com>
2021-06-28 01:22:44 +0700

    jt51platform: better USB wiring (at least the PHY responds, although timing fails)

--- gateware/jt51platform.py
+++ gateware/jt51platform.py
@@ -4,7 +4,7 @@ from nmigen.build import *
 from luna.gateware.platform.core import LUNAPlatform
 
 from nmigen_boards.resources import *
-from nmigen_boards.qmtech_xc7a35t_core import QMTechXC7A35TCorePlatform
+from nmigen_boards.qmtech_xc7a35t_core import QMTechXC7A35TPlatform
 
 
 class JT51SynthClockDomainGenerator(Elaboratable):
@@ -21,13 +21,20 @@ class JT51SynthClockDomainGenerator(Elaboratable):
         m.domains.usb  = ClockDomain()
         m.domains.fast = ClockDomain()
         m.domains.jt51 = ClockDomain()
+        m.domains.adat = ClockDomain()
 
         main_clock = Signal()
         fast_clock = Signal()
         jt51_clock = Signal()
+        adat_clock = Signal()
 
         locked   = Signal()
         feedback = Signal()
+
+        adat_feedback = Signal()
+        adat_locked = Signal()
+
+        clk_50 = platform.request(platform.default_clk)
 
         m.submodules.usb2_pll = Instance("PLLE2_ADV",
             p_BANDWIDTH            = "OPTIMIZED",
@@ -45,10 +52,28 @@ class JT51SynthClockDomainGenerator(Elaboratable):
             p_CLKIN1_PERIOD        = 20,
             i_CLKFBIN              = feedback,
             o_CLKFBOUT             = feedback,
-            i_CLKIN1               = platform.request(platform.default_clk),
+            i_CLKIN1               = clk_50,
             o_CLKOUT0              = main_clock,
             o_CLKOUT1              = fast_clock,
             o_LOCKED               = locked,
+        )
+
+        m.submodules.adat_pll = Instance("PLLE2_ADV",
+            p_BANDWIDTH            = "OPTIMIZED",
+            p_COMPENSATION         = "ZHOLD",
+            p_STARTUP_WAIT         = "FALSE",
+            p_DIVCLK_DIVIDE        = 1,
+            p_CLKFBOUT_MULT        = 29,
+            p_CLKFBOUT_PHASE       = 0.000,
+            p_CLKOUT0_DIVIDE       = 118,  # 12.288MHz
+            p_CLKOUT0_PHASE        = 0.000,
+            p_CLKOUT0_DUTY_CYCLE   = 0.500,
+            p_CLKIN1_PERIOD        = 20,
+            i_CLKFBIN              = adat_feedback,
+            o_CLKFBOUT             = adat_feedback,
+            i_CLKIN1               = clk_50,
+            o_CLKOUT0              = adat_clock,
+            o_LOCKED               = adat_locked,
         )
 
         led = platform.request("led")
@@ -75,13 +100,22 @@ class JT51SynthClockDomainGenerator(Elaboratable):
             ClockSignal("usb").eq(main_clock),
             ClockSignal("fast").eq(fast_clock),
             ClockSignal("jt51").eq(jt51_clock),
+            ClockSignal("adat").eq(adat_clock),
+            ResetSignal("sync").eq(locked & adat_locked),
+            ResetSignal("fast").eq(locked & adat_locked),
+            ResetSignal("jt51").eq(locked & adat_locked),
+            ResetSignal("adat").eq(locked & adat_locked),
+            ResetSignal("sync").eq(locked & adat_locked),
         ]
 
         platform.add_clock_constraint(main_clock, 60e6)
 
+        ground = platform.request("ground")
+        m.d.comb += ground.eq(0)
+
         return m
 
-class JT51SynthPlatform(QMTechXC7A35TCorePlatform, LUNAPlatform):
+class JT51SynthPlatform(QMTechXC7A35TPlatform, LUNAPlatform):
     clock_domain_generator = JT51SynthClockDomainGenerator
     default_usb_connection = "ulpi"
 
@@ -95,35 +129,36 @@ class JT51SynthPlatform(QMTechXC7A35TCorePlatform, LUNAPlatform):
                                  "set_input_delay -clock main_clock -max 3.5 $ulpi_inputs\n"
 
         jt51_files = [
-            "../gateware/jt51/hdl/filter/jt51_sincf.v",
-            "../gateware/jt51/hdl/filter/jt51_interpol.v",
-            "../gateware/jt51/hdl/filter/jt51_fir_ram.v",
-            "../gateware/jt51/hdl/filter/jt51_fir8.v",
-            "../gateware/jt51/hdl/filter/jt51_fir4.v",
-            "../gateware/jt51/hdl/filter/jt51_fir.v",
-            "../gateware/jt51/hdl/filter/jt51_dac2.v",
-            "../gateware/jt51/hdl/jt51_timers.v",
-            "../gateware/jt51/hdl/jt51_sh.v",
-            "../gateware/jt51/hdl/jt51_reg.v",
-            "../gateware/jt51/hdl/jt51_pm.v",
-            "../gateware/jt51/hdl/jt51_phrom.v",
-            "../gateware/jt51/hdl/jt51_phinc_rom.v",
-            "../gateware/jt51/hdl/jt51_pg.v",
-            "../gateware/jt51/hdl/jt51_op.v",
             "../gateware/jt51/hdl/jt51_noise_lfsr.v",
+            "../gateware/jt51/hdl/jt51_csr_ch.v",
+            "../gateware/jt51/hdl/jt51_phinc_rom.v",
+            "../gateware/jt51/hdl/deprecated/jt51_sh2.v",
+            "../gateware/jt51/hdl/jt51_phrom.v",
+            "../gateware/jt51/hdl/jt51_acc.v",
+            "../gateware/jt51/hdl/jt51_exp2lin.v",
+            "../gateware/jt51/hdl/jt51_op.v",
+            "../gateware/jt51/hdl/jt51_csr_op.v",
+            "../gateware/jt51/hdl/jt51_sh.v",
             "../gateware/jt51/hdl/jt51_noise.v",
             "../gateware/jt51/hdl/jt51_mod.v",
-            "../gateware/jt51/hdl/jt51_mmr.v",
-            "../gateware/jt51/hdl/jt51_lin2exp.v",
             "../gateware/jt51/hdl/jt51_lfo.v",
+            "../gateware/jt51/hdl/jt51_mmr.v",
             "../gateware/jt51/hdl/jt51_kon.v",
-            "../gateware/jt51/hdl/jt51_exprom.v",
-            "../gateware/jt51/hdl/jt51_exp2lin.v",
-            "../gateware/jt51/hdl/jt51_eg.v",
-            "../gateware/jt51/hdl/jt51_csr_op.v",
-            "../gateware/jt51/hdl/jt51_csr_ch.v",
-            "../gateware/jt51/hdl/jt51_acc.v",
+            "../gateware/jt51/hdl/jt51_lin2exp.v",
+            "../gateware/jt51/hdl/jt51_pg.v",
             "../gateware/jt51/hdl/jt51.v",
+            "../gateware/jt51/hdl/jt51_eg.v",
+            "../gateware/jt51/hdl/jt51_timers.v",
+            "../gateware/jt51/hdl/jt51_pm.v",
+            "../gateware/jt51/hdl/jt51_exprom.v",
+            "../gateware/jt51/hdl/filter/jt51_sincf.v",
+            "../gateware/jt51/hdl/filter/jt51_interpol.v",
+            "../gateware/jt51/hdl/filter/jt51_fir.v",
+            "../gateware/jt51/hdl/filter/jt51_fir_ram.v",
+            "../gateware/jt51/hdl/filter/jt51_fir8.v",
+            "../gateware/jt51/hdl/filter/jt51_dac2.v",
+            "../gateware/jt51/hdl/filter/jt51_fir4.v",
+            "../gateware/jt51/hdl/jt51_reg.v",
         ]
 
         plan.files['top.tcl'] = plan.files['top.tcl'].replace("add_files", "add_files " + " ".join(jt51_files))
@@ -134,11 +169,12 @@ class JT51SynthPlatform(QMTechXC7A35TCorePlatform, LUNAPlatform):
         self.resources += [
             # USB2 / ULPI section of the USB3300.
             ULPIResource("ulpi", 0,
-                data="J_2:28 J_2:26 J_2:24 J_2:22 J_2:20 J_2:18 J_2:16 J_2:14",
-                clk="J_2:9", # this needs to be a clock pin of the FPGA or the core won't work
-                dir="J_2:17", nxt="J_2:15", stp="J_2:13", rst="J_2:19", clk_dir="o",
-                rst_invert=True, # USB3320 reset is active low
+                data="J_2:31 J_2:29 J_2:27 J_2:25 J_2:23 J_2:21 J_2:19 J_2:17",
+                clk="J_2:9", clk_dir="o", # this needs to be a clock pin of the FPGA or the core won't work
+                dir="J_2:11", nxt="J_2:13", stp="J_2:15", rst="J_2:7", rst_invert=True, # USB3320 reset is active low
                 attrs=Attrs(IOSTANDARD="LVCMOS33", SLEW="FAST")),
+
+            Resource("ground", 0, Pins(" ".join([f"J_2:{i}" for i in range(8, 33, 2)]), dir="o"), Attrs(IOSTANDARD="LVCMOS33")),
 
             Resource("debug_led", 0, Pins("J_2:34", dir="o"), Attrs(IOSTANDARD="LVCMOS33")),
             Resource("debug_led", 1, Pins("J_2:36", dir="o"), Attrs(IOSTANDARD="LVCMOS33")),
